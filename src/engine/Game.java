f413805306c604efc899e9c83cab8386b4e128f2
Peter <Peter@Peter-LEGION>
2020-03-02 23:38:16 +0200

    we love you omar 5er

---
+++ src/engine/Game.java
@@ -0,0 +1,43 @@
+package engine;
+import model.heroes.*;
+import java.util.*;
+public class Game {
+	Hero firstHero;
+	Hero secondHero;
+	Hero currentHero;
+	Hero opponent; 
+	
+
+
+
+	public Game() {
+		
+	}
+	/*this constructor randomly chooses the starting hero giving him one mana*/
+	public Game(Hero p1, Hero p2) {
+		firstHero=p1;
+		secondHero=p1;
+		Random rand = new Random(); 
+		int x=rand.nextInt(2);
+		if(x==0) {
+			currentHero=firstHero;
+			opponent=secondHero;
+			firstHero.setCurrentManaCrystals(1);
+		}else {
+			currentHero=secondHero;
+			opponent=firstHero;
+			secondHero.setCurrentManaCrystals(1);
+		}
+	}
+	
+	
+	
+	public Hero getCurrentHero() {
+		return currentHero;
+	}
+	public Hero getOpponent() {
+		return opponent;
+	}
+	 
+
+}
